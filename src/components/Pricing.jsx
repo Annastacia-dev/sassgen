8e5b3e556b15b178463f68e09b7d180ab161f431
Annastacia Mumbua Kioko <anne@mradi.co.ke>
2023-12-30 11:53:06 +0300

    pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -11,7 +11,7 @@ const Pricing = () => {
         <img
           src="/fluid-shape.png"
           alt="pricing"
-          className="absolute w-72 top-96 -left-24"
+          className="absolute w-72 top-[26rem] -left-32"
         />
         <div className="flex flex-col sm:flex-row justify-center items-center gap-10 mt-10 relative">
           <div className="bg-gradient-to-r from-purple-400  to-purple-300 rounded-lg h-[32rem] w-64 shadow-lg">
@@ -106,6 +106,21 @@ const Pricing = () => {
                   Get Your 30 day free trial
                 </p>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-20 bg-gradient-to-r from-primary to-primaryDark rounded-lg w-full shadow-lg text-white relative p-16">
+          <div className="flex justify-between items-center">
+            <p className="font-bold w-1/2 text-2xl leading-10">
+              10,000+ Professionals are recommending SASS product
+            </p>
+            <div className="flex gap-4">
+              <button className="bg-gradient-to-r from-secondaryDark to-secondary text-white capitalize  px-6 py-4 rounded-full text-xs font-bold mt-2 cursor-pointer hover:bg-gradient-to-r hover:text-white hover:from-secondary hover:to-secondaryDark">
+                get started now
+              </button>
+              <button className="bg-transparent border text-white capitalize  px-6 py-4 rounded-full text-xs font-bold mt-2 cursor-pointer hover:bg-gradient-to-r hover:text-white hover:bg-white">
+                watch trailer
+              </button>
             </div>
           </div>
         </div>
